0cc714fd2d29ff15782cc4f607c02c076224ba9d
Ерошенко Александр <aeroshenko@diasoft.ru>
2020-04-30 15:22:03 +0300

    модуль 7

---
+++ react/app/src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Loader() {
+    return (
+        <div>
+            <h3 className="loader" >Loading...</h3> :
+        </div>
+    )
+}
+
+export default Loader
---
+++ react/app/src/todoData.ts
@@ -0,0 +1,25 @@
+const todoData = [
+    {
+        id: 1,
+        caption: "Поесть",
+        done: true
+    },
+    {
+        id: 2,
+        caption: "Поспать",
+        done: false
+
+    },
+    {
+        id: 3,
+        caption: "Попить",
+        done: false
+    },
+    {
+        id: 4,
+        caption: "Поработать",
+        done: false
+    }
+]
+
+export default todoData
